4d644ff9b3298732ddde3c065ace1847da34ba62
beatrizrpinheiro <beatrizp.gestaoemp@gmail.com>
2022-02-28 22:09:31 -0300

    Project Created

---
+++ Versality/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Versality.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
